6f9a976aa94ccf35dfa2889f4edec8c07a681c0f
Fabricourt <mfalme2030@gmail.com>
2019-06-08 14:06:35 +0300

    templates

--- flames/flames/settings.py
+++ flames/flames/settings.py
@@ -166,12 +166,12 @@ MESSAGE_TAGS = {
 
 #EMAIL_BACKEND = 'django.core.mail.backends.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mfalme2030@gmail.com'
-EMAIL_HOST_PASSWORD = 'whyilovemywife'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_USE_TLS = True
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'mfalme2030@gmail.com'
+#EMAIL_HOST_PASSWORD = 'whyilovemywife'
 
 
 
